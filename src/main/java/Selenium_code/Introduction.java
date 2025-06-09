a4812aa7e9f286d1e93f19a73feb4262fc2c3521
asus <swatikadam68@gmail.com>
2025-06-09 23:27:57 +0530

    Java with Selenium login page

--- src/main/java/Selenium_code/Introduction.java
+++ src/main/java/Selenium_code/Introduction.java
@@ -1,4 +1,4 @@
-package Selnium_code;
+package Selenium_code;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
--- src/main/java/Selenium_code/LoginTest.java
+++ src/main/java/Selenium_code/LoginTest.java
@@ -1,4 +1,4 @@
-package Selnium_code;
+package Selenium_code;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
