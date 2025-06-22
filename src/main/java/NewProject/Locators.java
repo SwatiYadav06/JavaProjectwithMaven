86cb6a5fad33051eec24dd869369f039d3638e33
asus <swatikadam68@gmail.com>
2025-06-23 00:05:57 +0530

    Added new selenium with maven project

--- src/main/java/NewProject/Locators.java
+++ src/main/java/NewProject/Locators.java
@@ -10,15 +10,15 @@ public class Locators {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver","D:\\SWATI\\New notes\\Java Programs\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
         WebDriver driver=new ChromeDriver();
-        driver.get("https://app.vwo.com/#/login");
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        driver.findElement(By.id("login-username")).sendKeys("Swati");
-        driver.findElement(By.id("login-password")).sendKeys("1234");
-        driver.findElement(By.id("js-login-btn")).click();
+        driver.findElement(By.name("username")).sendKeys("Swati");
+        driver.findElement(By.name("password")).sendKeys("swati@123");
+        driver.findElement(By.className("orangehrm-login-button")).click();
         System.out.println("Title: " + driver.getTitle());
-        System.out.println(driver.findElement(By.id("js-notification-box-msg")).getText());
+       // System.out.println(driver.findElement(By.className("oxd-alert-content-text")).getText());
 
-        driver.findElement(By.className("btn--primary")).click();
+        driver.findElement(By.className("orangehrm-login-forgot-header")).click();
          driver.close();
 
     }
--- src/main/java/NewProject/LoginBy_xpath.java
+++ src/main/java/NewProject/LoginBy_xpath.java
@@ -3,17 +3,24 @@ package NewProject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class LoginBy_xpath {
     public static void main(String[] args) {
         // XPath----------------
         System.setProperty("webdriver.chrome.driver","D:\\SWATI\\New notes\\Java Programs\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
         WebDriver driver=new ChromeDriver();
-        driver.findElement(By.xpath("//input[@id='login-username']")).sendKeys("Swati");
-        driver.findElement(By.xpath("//input[@id='login-password']")).sendKeys("12345");
-        driver.findElement(By.xpath("//button[@id='js-login-btn']")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("Swati");
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("12345");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
         System.out.println("Title: " + driver.getTitle());
-        System.out.println(driver.findElement(By.xpath("//button[@id='js-notification-box-msg']")).getText());
-        driver.close();
+        System.out.println(driver.findElement(By.xpath("//p[@class='oxd-text oxd-text--p oxd-alert-content-text']")).getText());
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.findElement(By.xpath("//p[@class='oxd-text oxd-text--p orangehrm-login-forgot-header']")).click();
+        //driver.close();
     }
 }
